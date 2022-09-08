1f901e76dc0f7d491a9698041fa2f188c5761656
thecolorpuce <rileyasp@gmail.com>
2022-09-08 14:08:32 -0500

    This file is errored out, and it was bothering me. WILL FIX LATER!

--- LectureNotes/PythonFunctionandFiles/scaniterator.py
+++ LectureNotes/PythonFunctionandFiles/scaniterator.py
@@ -1,5 +1,9 @@
 import os
 
-with os.scandir("C:\Users\AspRiley\OneDrive - University of Wisconsin-Stout\Desktop\Fall2022\") as entries:
-    for entry in entries:
-        print(entry.name)
+#with os.scandir("C:\Users\AspRiley\OneDrive - University of Wisconsin-Stout\Desktop\Fall2022\") as entries:
+#    for entry in entries:
+#        print(entry.name)
+
+#Lots of errors to be had here.
+
+#Makes me sad
