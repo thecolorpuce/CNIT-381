4a7815f153fd1792d289d80ed121e26c9fb87e01
thecolorpuce <rileyasp@gmail.com>
2022-09-10 15:10:51 -0500

    Added the solution to question 5

--- Lab2/ver1.py
+++ Lab2/ver1.py
@@ -171,4 +171,30 @@ print(f"\nF({n}) = {fibonacci(n)}")
 ### output [1,2,3,4,4,5,6,7,8,9]
 
 print ("Problem 5 solution follows:")
-# ... write your code and comments here (and remove this line)
+
+#A sorted buffer. I think we could make use of the zip function for this
+#I'll make use of the two provided lists above
+
+A = [1,3,4,8,9]
+B = [2,4,5,6,7]
+
+def sortedMerge(a, b):
+    """Sorted Merge
+    We're trying to numerically sort two lists
+    
+    INPUT:
+    Two equal length lists of numerics
+    
+    OUTPUT:
+    The contents of the two lists merged into one list, and sorted numerically"""
+    
+    for i in b:
+        output = i
+        a.append(output)
+    
+    a.sort()
+    return a
+
+A = sortedMerge(A, B)       #Call the function, and print
+
+print(A)
